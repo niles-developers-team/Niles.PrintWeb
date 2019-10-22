3078261a755235321979ff051fe2c2ac630e7d32
Kirill Ivanchenko <kni2012.15@gmail.com>
2019-10-22 23:47:49 +0300

    Appended creating not confirmed users table to initiliaisation migration;

--- Niles.PrintWeb.Utility/Migrations/20191022_1_CreateUser.cs
+++ Niles.PrintWeb.Utility/Migrations/20191022_1_CreateUser.cs
@@ -16,11 +16,18 @@ namespace Niles.PrintWeb.Utility.Migrations
                 .WithColumn("Email").AsString().NotNullable()
                 .WithColumn("DateCreated").AsDateTime().NotNullable().WithDefault(SystemMethods.CurrentUTCDateTime)
                 .WithColumn("DateUpdated").AsDateTime().NotNullable().WithDefault(SystemMethods.CurrentUTCDateTime);
+
+                
+            Create.Table("NotConfirmedUser")
+                .WithColumn("UserId").AsInt32().PrimaryKey().ForeignKey("User", "Id")
+                .WithColumn("ConfirmCode").AsGuid().NotNullable()
+                .WithColumn("DateCreated").AsDateTime().NotNullable().WithDefault(SystemMethods.CurrentUTCDateTime);
         }
         
         public override void Down()
         {
             Delete.Table("Users");
+            Delete.Table("NotConfirmedUser");
         }
     }
 }
