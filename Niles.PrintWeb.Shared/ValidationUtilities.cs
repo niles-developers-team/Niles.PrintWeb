581a7ddf6b1fb4845f20f4313c691e2d425de5ef
Kirill Ivanchenko <kni2012.15@gmail.com>
2020-01-28 02:10:07 +0300

    #5:FIX - Returned lost namespace;

--- Niles.PrintWeb.Shared/ValidationUtilities.cs
+++ Niles.PrintWeb.Shared/ValidationUtilities.cs
@@ -2,7 +2,7 @@ using System;
 using System.Globalization;
 using System.Text.RegularExpressions;
 
-namespace Niles.PrintWeb.Utilities
+namespace Niles.PrintWeb.Shared
 {
     public class ValidationUtilities
     {
