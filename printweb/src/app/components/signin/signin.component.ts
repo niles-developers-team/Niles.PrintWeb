fa9ec158e66841170a28addbcab2c181088e729d
Nickolay Panferov <mr.kolya.panferov@mail.ru>
2020-01-30 06:17:59 +0300

    #2:registration is ready;

--- printweb/src/app/components/signin/signin.component.ts
+++ printweb/src/app/components/signin/signin.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'sign-in',
     templateUrl: './signin.component.html',
-    styleUrls: ['./signin.component.css']
+    styleUrls: ['./signin.component.css', '../../app.component.css']
 })
 export class SignInComponent {
 
