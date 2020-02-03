13434bcdec3544f40d649c2c693768193785a69f
Kirill Ivanchenko <kni2012.15@gmail.com>
2020-02-03 22:55:06 +0300

    #14: Fixed migration number; Fixed table naming;

--- Niles.PrintWeb.Utilities/Migrations/20200202_3_CreateTenant.cs
+++ Niles.PrintWeb.Utilities/Migrations/20200202_3_CreateTenant.cs
@@ -2,19 +2,19 @@ using FluentMigrator;
 
 namespace Niles.PrintWeb.Utilities.Migrations
 {
-    [Migration(202002020002)]
+    [Migration(202002020003)]
     public class CreateTenant : ForwardOnlyMigration
     {
         public override void Up()
         {
-            Create.Table("Tenants")
+            Create.Table("Tenant")
                 .WithColumn("Id").AsInt32().PrimaryKey().Identity()
                 .WithColumn("Name").AsString().NotNullable()
                 .WithColumn("DateCreated").AsDateTime().NotNullable().WithDefault(SystemMethods.CurrentUTCDateTime)
                 .WithColumn("SubscriptionId").AsInt32().NotNullable().ForeignKey("Subscription", "Id")
                 .WithColumn("SubscribeDate").AsDateTime().NotNullable();
 
-            Create.Table("Offices")
+            Create.Table("Office")
                 .WithColumn("Id").AsInt32().PrimaryKey().Identity()
                 .WithColumn("Address").AsString().NotNullable()
                 .WithColumn("TenantId").AsInt32().NotNullable().ForeignKey("Tenant", "Id");
