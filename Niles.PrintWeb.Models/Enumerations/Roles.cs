a356e8fdaf9bc84c9fbe4d7f4888b165efb73c07
Kirill Ivanchenko <kni2012.15@gmail.com>
2020-02-04 02:36:45 +0300

    #22: Appended description attribute to user roles enum;

--- Niles.PrintWeb.Models/Enumerations/Roles.cs
+++ Niles.PrintWeb.Models/Enumerations/Roles.cs
@@ -1,13 +1,18 @@
+using System.ComponentModel;
+
 namespace Niles.PrintWeb.Models.Enumerations
 {
     ///<summary>Users roles enumeration.</summary>
     public enum Roles
     {
         ///<summary>Admin user, who control users and tenants colloboration.</summary>
+        [Description("Admin")]
         Admin,
         ///<summary>Tenant user, who can print files.</summary>
+        [Description("Tenant")]
         TenantUser,
         ///<summary>Client, who want to print his files.</summary>
+        [Description("Client")]
         Client
     }
 }
