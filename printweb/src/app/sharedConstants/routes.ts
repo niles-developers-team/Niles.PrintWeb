360033271787a8639019f3bca4ded96e8b276957
Kirill Ivanchenko <kni2012.15@gmail.com>
2020-02-05 20:37:27 +0300

    #16: Appended forbidden page to application routes;

--- printweb/src/app/sharedConstants/routes.ts
+++ printweb/src/app/sharedConstants/routes.ts
@@ -5,6 +5,7 @@ import { SignInComponent } from '../components/signin/signin.component';
 import { adminRoutes } from './adminRoutes';
 import { Routes, Route } from '@angular/router';
 import { AuthorizeGuard } from '../guards/authorize.guard';
+import { ForbiddenComponent } from '../components/common/forbidden/forbidden';
 
 const appRoutes: Routes = adminRoutes.map(o => {
     const route: Route = { path: o.path, component: o.component, canActivate: o.canActivate, outlet: o.outlet, data: { breadcrumb: o.text } };
@@ -15,5 +16,6 @@ export const routes: Routes = [
     { path: 'admin', redirectTo: 'admin/dash', pathMatch: 'full' },
     { path: 'admin', component: AdminComponent, canActivate: [AuthenticateGuard, AuthorizeGuard], children: appRoutes, data: { breadcrumb: 'Admin', parent: true } },
     { path: 'signup', component: SignUpComponent, canActivate: [AuthenticateGuard] },
-    { path: 'signin', component: SignInComponent, canActivate: [AuthenticateGuard] }
+    { path: 'signin', component: SignInComponent, canActivate: [AuthenticateGuard] },
+    { path: 'forbidden', component: ForbiddenComponent }
 ]
