48353f4dc777872ed58edc87dfa458359ef111db
Kirill Ivanchenko <kni2012.15@gmail.com>
2020-02-06 02:56:12 +0300

    #19: Created tenat model and tenant get options;

---
+++ Niles.PrintWeb.Models/Entities/TenantModels.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Niles.PrintWeb.Models.Entities
+{
+    public class Tenant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateCreated { get; set; }
+        public int SubscriptionId { get; set; }
+        public DateTime SubscribeDate { get; set; }
+    }
+
+    public class TenantGetOptions : BaseGetOptions
+    {
+        public string NormalizedSearch => !string.IsNullOrEmpty(Search) ? $"%{Search}%" : string.Empty;
+        public string Search { get; set; }
+    }
+}
