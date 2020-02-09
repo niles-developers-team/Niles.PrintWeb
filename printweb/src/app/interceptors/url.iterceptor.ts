4092bf3679375dc38eb64cf669c380601ace809a
Kirill Ivanchenko <kni2012.15@gmail.com>
2020-02-09 17:26:03 +0300

    #27: Fixed error with development api url on production;

--- printweb/src/app/interceptors/url.iterceptor.ts
+++ printweb/src/app/interceptors/url.iterceptor.ts
@@ -1,5 +1,5 @@
 import { HttpInterceptor, HttpRequest, HttpEvent, HttpHandler } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, isDevMode } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable()
@@ -7,7 +7,13 @@ export class ApiUrlInterceptor implements HttpInterceptor {
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         const { url } = request;
         const prefixRegex = /^\/?api\//;
-        const apiUrl = 'http://localhost:5000'
+        let apiUrl = '';
+        
+        if (isDevMode)
+            apiUrl = 'http://localhost:5000';
+        else
+            apiUrl = 'https://printweb-api.azurewebsites.net/';
+        
         let newUrl: string;
         const matches = prefixRegex.exec(url);
         if (matches && matches.length > 0) {
