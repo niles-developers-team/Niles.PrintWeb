53e342474be76483964bf928b844f6bd9cee6638
Nickolay Panferov <mr.kolya.panferov@mail.ru>
2020-02-12 23:04:30 +0300

    #8:Added table;
    #8:Added button "edit";

--- printweb/src/app/components/admin/users/users.component.ts
+++ printweb/src/app/components/admin/users/users.component.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
+import { IUser } from 'src/app/models/user.model';
 
+
+const ELEMENT_DATA: IUser[] = [
+    { username: 'A', email: '', firstName: '', lastName: '', password: null},
+    { username: 'B', email: '', firstName: 'He', lastName: '', password: null},
+    { username: 'C', email: '', firstName: 'Li', lastName: '', password: null},
+    { username: 'D', email: '', firstName: 'Be', lastName: '', password: null},
+    { username: 'E', email: '', firstName: 'B', lastName: '', password: null},
+    { username: 'F', email: '', firstName: 'C', lastName: '', password: null},
+    { username: 'G', email: '', firstName: 'N', lastName: '', password: null},
+    { username: 'H', email: '', firstName: 'O', lastName: '', password: null},
+    { username: 'I', email: '', firstName: 'F', lastName: '', password: null},
+    { username: 'J', email: '', firstName: 'Ne', lastName: '', password: null},
+];
+
+/**
+ * @title Basic use of `<table mat-table>`
+ */
 @Component({
     selector: 'users',
-    templateUrl: './users.component.html'
+    templateUrl: './users.component.html',
+    styleUrls: ['./users.component.css', '../../../app.component.scss']
 })
 export class UsersComponent {
-    //TODO: Need to implement this component
+    displayedColumns: string[] = ['username', 'edit', 'email', 'firstName', 'lastName'];
+    users = ELEMENT_DATA;
 }
