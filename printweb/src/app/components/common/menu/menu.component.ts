4c7928a94834d6bd719051ac4ac711853f3079b9
Kirill Ivanchenko <kni2012.15@gmail.com>
2020-02-16 01:55:28 +0300

    #16: Fixed issue with not deleted current user after sign out;

--- printweb/src/app/components/common/menu/menu.component.ts
+++ printweb/src/app/components/common/menu/menu.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from "@angular/core";
 import { MenuItem } from 'src/app/models/menuItem.model';
+import { UserService } from 'src/app/services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'main-menu',
@@ -9,9 +11,20 @@ import { MenuItem } from 'src/app/models/menuItem.model';
 export class MenuComponent {
     @Input() public items: MenuItem[] = [];
 
+    constructor(private readonly _userServce: UserService,
+        private readonly _router: Router
+    ) {
+
+    }
+
     public collapsed: boolean = true;
 
     public onLogoClick() {
         this.collapsed = !this.collapsed;
+    }
+
+    public signOut() {
+        this._userServce.signout();
+        this._router.navigateByUrl('/signin');
     }
 }
