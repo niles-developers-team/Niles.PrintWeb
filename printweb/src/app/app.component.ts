0ddc3bc0fe30aa9d9c8b6652c0a815894397c6aa
Kirill Ivanchenko <kni2012.15@gmail.com>
2020-02-18 00:25:25 +0300

    #29: Created new UI style;

--- printweb/src/app/app.component.ts
+++ printweb/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MenuItem } from './models/menuItem.model';
+import { RouteService } from './services/route.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'printweb';
+  public readonly routes: MenuItem[];
+  public readonly dashRoutes: MenuItem;
+
+  constructor(private readonly _routeService: RouteService) {
+    const routes = this._routeService.enabledMenuItems;
+    this.routes = routes.slice(1);
+    this.dashRoutes = routes[0];
+  }
 }
