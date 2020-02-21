1fdadd9bf6aca92b22040768b904292d0c35222d
Kirill Ivanchenko <kni2012.15@gmail.com>
2020-02-21 21:55:52 +0300

    #29: Added parsing command line arguments;

--- Niles.PrintWeb.Utilities/Program.cs
+++ Niles.PrintWeb.Utilities/Program.cs
@@ -14,14 +14,37 @@ namespace Niles.PrintWeb.Utilities
     {
         public static int Main(string[] args)
         {
-            return 0;
-            ///
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
 
-            fefe///
+            DatabaseConnectionSettings settings = JsonConvert.DeserializeObject<DatabaseConnectionSettings>(
+                File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json"))
+            );
 
+            var services = serviceCollection.BuildServiceProvider();
 
-            ///
-            efef
+            return CommandLine.Parser.Default.ParseArguments<MigrateUpOptions, MigrateDownOptions, DropOptions, CreateOptions, ResetOptions, SetSettingsOptions>(args)
+            .MapResult(
+                (DropOptions options) => RunDrop(services.GetService<ILogger<Drop>>(), settings),
+                (ResetOptions options) => RunReset(services.GetService<ILogger<Reset>>(), settings, options),
+                (CreateOptions options) => RunCreate(services.GetService<ILogger<Create>>(), settings),
+                (MigrateUpOptions options) => RunMigrateUp(services.GetService<ILogger<MigrateUp>>(), settings),
+                (MigrateDownOptions options) => RunMigrateDown(services.GetService<ILogger<MigrateDown>>(), settings),
+                (SetSettingsOptions options) => RunSettingsUpdate(services.GetService<ILogger<SettingsUpdate>>(), options),
+                errors => 1
+            );
         }
+
+        private static void ConfigureServices(ServiceCollection services)
+        {
+            services.AddLogging(builder => builder.AddConsole());
+        }
+
+        static int RunDrop(ILogger logger, DatabaseConnectionSettings settings) => Drop.Run(logger, settings);
+        static int RunReset(ILogger logger, DatabaseConnectionSettings settings, ResetOptions options) => Reset.Run(logger, settings, options);
+        static int RunCreate(ILogger logger, DatabaseConnectionSettings settings) => Create.Run(logger, settings);
+        static int RunMigrateUp(ILogger logger, DatabaseConnectionSettings settings) => MigrateUp.Run(logger, settings);
+        static int RunMigrateDown(ILogger logger, DatabaseConnectionSettings settings) => MigrateDown.Run(logger, settings);
+        static int RunSettingsUpdate(ILogger logger, SetSettingsOptions options) => SettingsUpdate.Run(logger, options);
     }
 }
